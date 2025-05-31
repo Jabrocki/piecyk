94f664e62a77030b7ee14165af02ae34c9ec0426
prbartosh <pajorb4@gmail.com>
2025-05-31 17:23:55 +0200

    complete weather model

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -1,1 +1,17 @@
+class WeatherModel {
+  final double time;
+  final double dni; //direct normal irradiance DNI W/m^2
+  final double dif; // diffuse solar radiation DIF W/m^2
+  final double ghi; // shortwave solar radiation GHI W/m^2
+  final double temp; // apparent_temperature °C
+  final double windSpeed; // wind speed 100m (cokolwiek znaczy 100m xd) km/h
 
+  WeatherModel({
+    required this.time,
+    required this.dni,
+    required this.dif,
+    required this.ghi,
+    required this.temp,
+    required this.windSpeed,
+  });
+}
